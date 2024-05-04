d7522472deb1aff4dd6c83cee82e175d168b48dd
Joshua Kaddu <joshuakaddu@Joshuas-MacBook-Air.local>
2024-05-04 08:40:15 -0500

    server implementation

---
+++ TUT01/server.js
@@ -0,0 +1,13 @@
+// How NodeJS differs from Vanilla JS
+// NodeJS runs on a server compared to VanillaJA which runs in the browser
+// THe console is the termianl window compared to the console window on the brower
+// Has global object instead of a window object
+// Has core modules that I'll explore
+// Use ComminJS modules instead of ES6 modules
+
+const os = require(os);
+
+// console.log(os.type());
+// console.log(os.version());
+// console.log(os.homedir());
+console.log("Hello World");
