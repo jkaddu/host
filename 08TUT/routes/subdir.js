b2b5602daffbf7cbe787ef4f1fa8f8afbb00a08b
Joshua Kaddu <joshuakaddu@Joshuas-MacBook-Air.local>
2024-05-09 10:40:21 -0500

    created a routes for the subdir files

--- 08TUT/routes/subdir.js
+++ 08TUT/routes/subdir.js
@@ -3,7 +3,11 @@ const router = express.Router();
 const path = require("path");
 
 router.get("^/$|/index(.html)?", (req, res) => {
-  res.sendFile(path.join(__dirname, "views", "index.html"));
+  // ".." goes 1 level up into the root directory aka folder then goes into the views folder then the subdir folder and lastly the index.html file
+  res.sendFile(path.join(__dirname, "..", "views", "subdir", "index.html"));
+});
+router.get("^/test(.html)?", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "views", "subdir", "test.html"));
 });
 
 module.exports = router;
