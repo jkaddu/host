c6f5fbabad724310f48f73bf4cae92e20a12be32
Joshua Kaddu <joshuakaddu@Joshuas-MacBook-Air.local>
2024-05-11 18:38:45 -0500

    created JWTs in authController/created verifyJWT file

--- 11TUT/controllers/authController.js
+++ 11TUT/controllers/authController.js
@@ -9,6 +9,7 @@ const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
 const fsPromises = require("node:fs/promises");
+const path = require("path");
 
 const handleLogin = async (req, res) => {
   const { user, pwd } = req.body;
@@ -22,7 +23,37 @@ const handleLogin = async (req, res) => {
   const match = await bcrypt.compare(pwd, foundUser.password);
   if (match) {
     // create JWTs
-    res.json({ success: `User ${user} is logged in!` });
+    const accessToken = jwt.sign(
+      {
+        username: foundUser.username,
+      },
+      process.env.ACCESS_TOKEN_SECRET,
+      { expiresIn: "30s" }
+    );
+    const refreshToken = jwt.sign(
+      {
+        username: foundUser.username,
+      },
+      process.env.REFRESH_TOKEN_SECRET,
+      { expiresIn: "1d" }
+    );
+    // Saving refreshTokem with currentUser
+    const otherUsers = usersDB.users.filter(
+      (person) => person.username !== foundUser.username
+    );
+    const currentUser = { ...foundUser, refreshToken };
+    usersDB.setUsers([...otherUsers, currentUser]);
+    // Creating new users.json file with currentUser
+    await fsPromises.writeFile(
+      path.join(__dirname, "..", "model", "users.json"),
+      JSON.stringify(usersDB.users)
+    );
+    // Sends cookie that accepts httpOnly and last for a day aka 24 hours
+    res.cookie("jwt", refreshToken, {
+      httpOnly: true,
+      maxAge: 24 * 60 * 60 * 1000,
+    });
+    res.json({ accessToken });
   } else {
     res.sendStatus(401);
   }
