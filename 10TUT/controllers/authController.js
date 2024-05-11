1532233b0579c1472bd99801ac1a2a06c77bae4b
Joshua Kaddu <joshuakaddu@Joshuas-MacBook-Air.local>
2024-05-11 12:00:08 -0500

    logic for login created

---
+++ 10TUT/controllers/authController.js
@@ -0,0 +1,27 @@
+const usersDB = {
+  users: require("../model/users.json"),
+  setUsers: function (data) {
+    this.users = data;
+  },
+};
+
+const bcrpyt = require("bcrpyt");
+
+const handleLogin = async (req, res) => {
+  const { user, pwd } = req.body;
+  if (!user || !pwd)
+    return res
+      .status(400)
+      .json({ message: "Username and password are required." });
+  const foundUser = usersDB.users.find((person) => person.username === user);
+  if (!foundUser) return res.sendStatus(401); // status code of unauthorized
+  // evaluate passowrd
+  const match = await bcrpyt.compare(pwd, foundUser.password);
+  if (match) {
+    res.json({ success: `User ${user} is logged in!` });
+  } else {
+    res.sendStatus(401);
+  }
+};
+
+module.exports = { handleLogin };
