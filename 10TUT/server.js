8533a6a3df9412aea0ba8cb2b38aa65a066957d8
Joshua Kaddu <joshuakaddu@Joshuas-MacBook-Air.local>
2024-05-11 10:56:53 -0500

    created users.json file and a registerController.js file

--- 10TUT/server.js
+++ 10TUT/server.js
@@ -37,7 +37,7 @@ app.all("*", (req, res) => {
     res.type("txt").send("Not Found");
   }
 });
-// Error handling middleware
+// Error handling custom middleware
 app.use(errorHandler);
 
 app.listen(PORT, () => console.log(`Server running on ${PORT}`));
