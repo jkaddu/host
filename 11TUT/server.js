16877225e2c701e899c273bcf6a46681b536fd8d
Joshua Kaddu <joshuakaddu@Joshuas-MacBook-Air.local>
2024-05-11 17:37:31 -0500

    created JWT tutorial folder

---
+++ 11TUT/server.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const cors = require("cors");
+const corsOptions = require("./config/corsOptions");
+const { logger } = require("./middleware/logEvents");
+const errorHandler = require("./middleware/errorHandler");
+const PORT = process.env.PORT || 3500;
+
+// Custom middleware logger
+app.use(logger);
+
+// Cross Origin Resource Sharing
+app.use(cors(corsOptions));
+
+// built in middleware to handle urlencoded data aka form data
+app.use(express.urlencoded({ extended: false }));
+
+// built in middkesware for json
+app.use(express.json());
+
+// serve static files aka applies the css,image or images and text
+app.use("/", express.static(path.join(__dirname, "/public")));
+
+// ROUTES
+app.use("/", require("./routes/root"));
+app.use("/auth", require("./routes/auth"));
+app.use("/register", require("./routes/register"));
+app.use("/employees", require("./routes/api/employees"));
+
+// Catch all for all for pages that don't exist
+app.all("*", (req, res) => {
+  res.status(404);
+  if (req.accepts("html")) {
+    res.sendFile(path.join(__dirname, "views", "404.html"));
+  } else if (req.accepts("json")) {
+    res.json({ error: "404 Not Found" });
+  } else {
+    res.type("txt").send("Not Found");
+  }
+});
+// Error handling custommiddleware
+app.use(errorHandler);
+
+app.listen(PORT, () => console.log(`Server running on ${PORT}`));
