24fdfdfcaf815d61f82bcac1afcc1cc19865bfc7
Joshua Kaddu <joshuakaddu@Joshuas-MacBook-Air.local>
2024-05-11 20:18:05 -0500

    refreshToken logic working now

--- 11TUT/controllers/refreshTokenController.js
+++ 11TUT/controllers/refreshTokenController.js
@@ -9,7 +9,7 @@ const jwt = require("jsonwebtoken");
 require("dotenv").config();
 
 const handleRefreshToken = (req, res) => {
-  const cookies = req.coookies;
+  const cookies = req.cookies;
   if (!cookies?.jwt) return res.sendStatus(401);
   console.log(cookies.jwt);
   const refreshToken = cookies.jwt;
--- 11TUT/server.js
+++ 11TUT/server.js
@@ -31,6 +31,7 @@ app.use("/", express.static(path.join(__dirname, "/public")));
 // ROUTES
 app.use("/", require("./routes/root"));
 app.use("/auth", require("./routes/auth"));
+app.use("/refresh", require("./routes/refresh"));
 app.use("/register", require("./routes/register"));
 
 app.use(verifyJWT);
