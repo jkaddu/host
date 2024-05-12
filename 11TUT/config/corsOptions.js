1ee35e780cc9c29c154377598805f07d9bfdb7b7
Joshua Kaddu <joshuakaddu@Joshuas-MacBook-Air.local>
2024-05-12 09:34:25 -0500

    put allowedOrigins aka whiteList into its own file

--- 11TUT/config/corsOptions.js
+++ 11TUT/config/corsOptions.js
@@ -1,14 +1,9 @@
-// Crsoo Orgin Resource Sharing
-const mylist = [
-  "https://www.google.com",
-  "http://127.0.0.1:5500",
-  "http://localhost:3500",
-];
+const allowedOrigins = require("./allowedOrigins");
 
 const corsOptions = {
   origin: (origin, callback) => {
     // this statement is saying if the domain is in my list show it in the browser
-    if (mylist.indexOf(origin) !== -1 || !origin) {
+    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
       callback(null, true);
     } else {
       callback(new Error("Not allowed by cors"));
