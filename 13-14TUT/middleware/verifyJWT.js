06c9003f0cd87cf638349fa5380c00d6faef33c6
Joshua Kaddu <joshuakaddu@Joshuas-MacBook-Air.local>
2024-05-18 10:01:46 -0500

    changed folder name to 13-14TUT

---
+++ 13-14TUT/middleware/verifyJWT.js
@@ -0,0 +1,16 @@
+const jwt = require("jsonwebtoken");
+
+const verifyJWT = (req, res, next) => {
+  const authHeaders = req.headers.authorization || req.headers.Authoorization;
+  if (!authHeaders?.startsWith("Bearer ")) return res.sendStatus(401);
+
+  const token = authHeaders.split(" ")[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) return res.sendStatus(403); // status code of forbidden/invalid token
+    req.user = decoded.UserInfo.username;
+    req.roles = decoded.UserInfo.roles;
+    next();
+  });
+};
+
+module.exports = verifyJWT;
